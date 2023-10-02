bcda7e29a1f51881c4150f59844a90a21d7645ba
Sathish <kalingantech@gmail.com>
2023-10-02 12:30:42 +0530

    Second commit

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        String time = String.valueOf(calendar.getTime());
+        Toast.makeText(MainActivity.this,time,Toast.LENGTH_LONG).show();
+
     }
 }
